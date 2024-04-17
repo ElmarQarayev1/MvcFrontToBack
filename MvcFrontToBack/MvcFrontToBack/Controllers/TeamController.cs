cbf88d479ff7aa702a3813f267ae51669b488be5
ElmarQarayev1 <elmareg@code.edu.az>
2024-04-18 01:03:35 +0400

    front hissesi yazildi

---
+++ MvcFrontToBack/MvcFrontToBack/Controllers/TeamController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcFrontToBack.Controllers
+{
+	public class TeamController:Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		
+	}
+}
+
